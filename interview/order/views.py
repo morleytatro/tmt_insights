e309d7c145afcdf5910cfedcf74c4c085e1a0094
Morley Tatro <morleytatro@gmail.com>
2023-08-25 11:31:58 -0700

    add order tags by order ID

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +15,9 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class OrderTagListView(APIView):
+    def get(self, request, pk):
+        order = Order.objects.get(pk=pk)
+        serializer = OrderTagSerializer(order.tags, many=True)
+        return Response(serializer.data)
